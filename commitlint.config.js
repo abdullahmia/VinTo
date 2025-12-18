9859b0cb801334e0fad21e342a1870c0f00b2f09
abdullahmia <abdullah149161@bsdi-bd.org>
2025-12-18 18:28:24 +0600

    chore: setup husky and commitlint for conventional commits

---
+++ commitlint.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',     // New feature
+        'fix',      // Bug fix
+        'docs',     // Documentation changes
+        'style',    // Code style changes (formatting, etc.)
+        'refactor', // Code refactoring
+        'perf',     // Performance improvements
+        'test',     // Adding or updating tests
+        'build',    // Build system or external dependencies
+        'ci',       // CI/CD changes
+        'chore',    // Other changes (maintenance, etc.)
+        'revert'    // Revert a previous commit
+      ]
+    ],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'never', ['upper-case', 'pascal-case']],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never']
+  }
+};
